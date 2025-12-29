e72d336f6a7ada08b75996f64a8b1b5c068a8966
HieuNV108 <117250329+hieunv-ict@users.noreply.github.com>
2025-12-29 15:10:55 +0700

    Change properties of ProductItem class

--- src/item-manager/itemData.js
+++ src/item-manager/itemData.js
@@ -3,21 +3,21 @@ import { renderProductItems } from "../cart-feature/productItemDOM";
 import { addToCart } from "../cart-feature/cartManager";
 
 // Import images
-let bananaImg = require("../assets/resources/thumb-bananas.png");
-let milkImg = require("../assets/resources/thumb-milk.png");
-let tomatoImg = require("../assets/resources/thumb-tomatoes.png");
-let ketchupImg = require("../assets/resources/thumb-tomatoketchup.png");
+let bananaImg = require("../assets/product-thumbnails/bananas.png");
+let milkImg = require("../assets/product-thumbnails/milk.png");
+let tomatoImg = require("../assets/product-thumbnails/tomatoes.png");
+let ketchupImg = require("../assets/product-thumbnails/tomatoketchup.png");
 
 // Mock data object
 const mockProductData = [
-  { id: "1", name: "Fresh Organic Bananas", price: 5, imgSrc: bananaImg },
-  { id: "2", name: "Whole Milk 1 Gallon", price: 10, imgSrc: milkImg },
-  { id: "3", name: "Organic Red Tomatoes", price: 8, imgSrc: tomatoImg },
-  { id: "4", name: "Premium Tomato Ketchup", price: 6, imgSrc: ketchupImg },
-  { id: "5", name: "Fresh Green Bananas", price: 4.5, imgSrc: bananaImg },
-  { id: "6", name: "Low Fat Milk", price: 9, imgSrc: milkImg },
-  { id: "7", name: "Cherry Tomatoes Pack", price: 7, imgSrc: tomatoImg },
-  { id: "8", name: "Organic Ketchup", price: 7.5, imgSrc: ketchupImg },
+  { id: "1", name: "Fresh Organic Bananas", price: 5, imgSrc: bananaImg, category:"Fruits", description:"",soldThisMonth: 10, soldthisYear: 100 },
+  // { id: "2", name: "Whole Milk 1 Gallon", price: 10, imgSrc: milkImg },
+  // { id: "3", name: "Organic Red Tomatoes", price: 8, imgSrc: tomatoImg },
+  // { id: "4", name: "Premium Tomato Ketchup", price: 6, imgSrc: ketchupImg },
+  // { id: "5", name: "Fresh Green Bananas", price: 4.5, imgSrc: bananaImg },
+  // { id: "6", name: "Low Fat Milk", price: 9, imgSrc: milkImg },
+  // { id: "7", name: "Cherry Tomatoes Pack", price: 7, imgSrc: tomatoImg },
+  // { id: "8", name: "Organic Ketchup", price: 7.5, imgSrc: ketchupImg },
 ];
 
 // Map mock data to ProductItem instances
@@ -27,14 +27,14 @@ export let bestSellingProducts = [];
 
 // Map trending products
 mockProductData.forEach((data) => {
-  const product = new ProductItem(data.id, data.name, data.price, data.imgSrc);
+  const product = new ProductItem(data.id, data.name, data.price, data.imgSrc, data.category, data.description, data.soldThisMonth, data.soldthisYear);
   itemList[data.id] = product;
   trendingProducts.push(product);
 });
 
 // Map best selling products
 mockProductData.forEach((data) => {
-  const product = new ProductItem(data.id, data.name, data.price, data.imgSrc);
+  const product = new ProductItem(data.id, data.name, data.price, data.imgSrc, data.category, data.description, data.soldThisMonth, data.soldthisYear);
   itemList[data.id] = product;
   bestSellingProducts.push(product);
 });
